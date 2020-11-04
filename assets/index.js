44a068b15f01838da66fd9fd9555cc4ed1b6f17a
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-11-04 10:26:45 -0600

    mute button added

--- assets/index.js
+++ assets/index.js
@@ -2,6 +2,8 @@ import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video');
-const button = document.querySelector('button');
+const playButton = document.getElementById('playButton')
+const muteButton = document.getElementById('muteButton');
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
-button.onclick = () => player.isPaused()? player.play(): player.pause()
+playButton.onclick = () => player.isPaused()? player.play(): player.pause()
+muteButton.onclick = () => player.media.muted ? player.unmute() : player.mute()
