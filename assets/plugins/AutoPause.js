c2f1e1c7c51d944b68886ae524a51e261d3c4fdb
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-11-10 11:45:55 -0600

    Add visibilityChange listener to pause video is the user is not watching the tab

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor(){
         this.threshold = 0.25
         this.handleIntersection = this.handleIntersection.bind(this)
+        this.handleVisibility = this.handleVisibility.bind(this)
     }
 
     run(player){
@@ -11,11 +12,22 @@ class AutoPause {
         })
 
         observer.observe(this.player.media)
+
+        document.addEventListener("visibilitychange",this.handleVisibility)
     }
 
     handleIntersection(entries){
         const entry = entries[0]
         const isVisible = entry.intersectionRatio >= this.threshold
+        if (isVisible){
+            this.player.play()
+        }else{
+            this.player.pause()
+        }
+    }
+
+    handleVisibility(){
+        const isVisible = document.visibilityState === "visible"
         if (isVisible){
             this.player.play()
         }else{
