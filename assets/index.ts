7b77203afb4eb92fe7fe95924db47ba6bc7a23d2
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-11-11 11:08:51 -0600

    Plugin to show ads created and implemented

--- assets/index.ts
+++ assets/index.ts
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import AdsPlugin from './plugins/Ads/index'
 
 const video = document.querySelector('video');
 const playButton: HTMLElement = document.getElementById('playButton')
 const muteButton: HTMLElement = document.getElementById('muteButton');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(),new AutoPause()] });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(),new AutoPause(), new AdsPlugin()] });
 playButton.onclick = () => player.isPaused()? player.play(): player.pause()
 muteButton.onclick = () => player.media.muted ? player.unmute() : player.mute()
 
