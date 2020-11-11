303ec31b72a1c617818e9e5ae638add3a1468a36
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-11-11 11:25:52 -0600

    show ads on video container

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,14 +1,25 @@
 class MediaPlayer {
     media: HTMLMediaElement
     plugins: Array<any>
+    container: HTMLElement
+
     constructor(config) {
         this.media = config.el;
         this.plugins = config.plugins || [];
+        this.initPlayer()
         this.initPlugins();
     }
     play() {
         this.media.play();
     }
+
+    private initPlayer(){
+        this.container = document.createElement('div')
+        this.container.style.position = 'relative'
+        this.media.parentNode.insertBefore(this.container,this.media)
+        this.container.appendChild(this.media)
+    }
+
     private initPlugins() {
         this.plugins.forEach(plugin => {
             plugin.run(this);
